b2601257f0059f765216660100dfc3152e7e4413
JiaCheng-code <675232630@qq.com>
2022-12-04 23:23:32 +0800

    feat:第一部分基础样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import FirstSection from "./sections/FirstSection";
+import SecondSection from "./sections/SecondSection";
+import ThirdSection from "./sections/ThirdSection";
+import styles from "./styles.module.scss"
 
 function App() {
-  return (
-    <div className="App">
-      app
-    </div>
-  );
+    return (
+        <div className={styles.app}>
+            <FirstSection/>
+            <SecondSection/>
+            <ThirdSection/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/sections/SecondSection/index.tsx
@@ -0,0 +1,10 @@
+import {FC} from "react";
+
+const SecondSetion:FC= ()=>{
+    return (
+        <div>
+            SecondSetion
+        </div>
+    )
+}
+export  default SecondSetion
